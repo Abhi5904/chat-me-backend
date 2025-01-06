041cd067f9d3b67bee08783aae2c41a1dfb06ac2
Abhi5904 <abhivagadiya456@gmail.com>
2025-01-06 18:02:04 +0530

    setup socket.io connection

--- src/socket-io/socket.ts
+++ src/socket-io/socket.ts
@@ -10,7 +10,23 @@ const initializeSocket = (server) => {
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
       credentials: true,
     },
+    pingTimeout: 60000,
+    connectTimeout: 60000,
+    // Add transport options
+    transports: ['websocket', 'polling'],
   });
+  io.on('connection', (socket) => {
+    console.log('Client connected:', socket.id);
+
+    socket.on('disconnect', () => {
+      console.log('Client disconnected:', socket.id);
+    });
+
+    socket.on('connect_error', (error) => {
+      console.error('Connection error:', error);
+    });
+  });
+
   console.log('Socket.IO initialized');
 };
 
