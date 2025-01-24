facd56015040f9b15ce18ebb9b5fbc9af266648e
Abhi5904 <abhivagadiya456@gmail.com>
2025-01-24 15:49:19 +0530

    Add cors origin

--- src/socket-io/socket.ts
+++ src/socket-io/socket.ts
@@ -6,14 +6,14 @@ let io;
 const initializeSocket = (server) => {
   io = new Server(server, {
     cors: {
-      origin: [
-        'http://localhost:3000',
-        'https://chat-me-frontend.vercel.app',
-        'https://chat-me-backend-theta.vercel.app',
-      ], // Adjust based on your frontend URL
+      // origin: ['http://localhost:3000', 'https://chat-me-frontend.vercel.app'],
+      origin: '*',
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
       credentials: true,
     },
+    path: '/socket',
+    wsEngine: ['ws', 'wss'],
+    allowEIO3: true,
     pingTimeout: 60000,
     connectTimeout: 60000,
     // Add transport options
